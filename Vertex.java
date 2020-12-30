faa84a27e42035b2de523de8fd34d87dc8bf3bff
asianur <43776916+asianur@users.noreply.github.com>
2020-12-30 16:07:03 +0800

    Create Vertex.java

---
+++ Vertex.java
@@ -0,0 +1,20 @@
+package cobasiah;
+
+public class Vertex {
+    String label;
+    double x;
+    double y;
+
+
+    public Vertex(String label, double x, double y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+
+    @Override
+    public String toString() {
+        return label + " (" + x + " , " + y + ")";
+    }
+}
