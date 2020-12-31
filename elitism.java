fe76e444ce4cf424637929978163f7ca22a6d57c
asianur <43776916+asianur@users.noreply.github.com>
2020-12-31 22:17:33 +0800

    Update elitism.java

--- elitism.java
+++ elitism.java
@@ -1,1 +1,22 @@
+package cobasiah;
 
+import java.io.File;
+
+public class elitism {
+    public static void main(String[] args) {
+        File file = new File("burma14.tsp");
+        Vertex[] arrayVertex = DataReader.read(file);
+        Data data = new Data(arrayVertex);
+        System.out.println(data.toString());
+        
+        Individu indv01 = new Individu(data);
+        indv01.generateRandomKromosom();
+        indv01.hitungNilaiFitness();
+        
+        System.out.print("TOUR: ");
+        System.out.println(indv01.toString());
+        System.out.println("total distance / jarak: "+indv01.totalJarak);
+        System.out.println("fitness: "+indv01.nilaiFitness);
+        
+    }
+}
